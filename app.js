d0f665fac88d53a97bc73e23778fa2b20423feee
rajuyd-amalgamrx <drajashekar@amalgamrx.com>
2026-05-15 18:02:23 +0530

    add ec2 deployement

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({ message: 'CI/CD Working!' });
+  res.json({ message: 'Full CI/CD Pipeline Working!' });
 });
 
 module.exports = app;
